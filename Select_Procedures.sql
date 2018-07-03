45d5db7552e0d8f18d8605e021a439bad081cc88
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-03 15:18:45 -0500

    Adición de procedimiento
    Adición de procedimiento para Login del sistema

--- Select_Procedures.sql
+++ Select_Procedures.sql
@@ -1,1 +1,8 @@
+              /* Procedimiento de validación de usuarios para Login del sistema */
+              
+DELIMITER $$
+CREATE PROCEDURE validacion(user varchar(20), pass varchar(25))
+BEGIN
+SELECT id_usuario,nombre,apellido,rol,foto_perfil FROM usuarios where correo = user and contrasenia = pass;
+END $$
 

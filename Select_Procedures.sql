b137581167c32d653c369f2e3b0e8491f51f57aa
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-03 15:20:38 -0500

    Update Select_Procedures.sql

--- Select_Procedures.sql
+++ Select_Procedures.sql
@@ -1,7 +1,7 @@
               /* Procedimiento de validación de usuarios para Login del sistema */
               
 DELIMITER $$
-CREATE PROCEDURE validacion(user varchar(20), pass varchar(25))
+CREATE PROCEDURE validacion_user(user varchar(20), pass varchar(25))
 BEGIN
 SELECT id_usuario,nombre,apellido,rol,foto_perfil FROM usuarios where correo = user and contrasenia = pass;
 END $$

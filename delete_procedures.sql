3c7c1942040f2dbfd9f1ebadd262220ff55ff27d
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-16 09:56:19 -0500

    Adición de un nuevo procedimiento

--- delete_procedures.sql
+++ delete_procedures.sql
@@ -1,1 +1,6 @@
-
+                                  /* Procedimiento para eliminar usuarios */
+DELIMITER $$ 
+CREATE PROCEDURE delete_user(id int)
+BEGIN
+DELETE FROM usuarios where id_usuario = id;
+END$$

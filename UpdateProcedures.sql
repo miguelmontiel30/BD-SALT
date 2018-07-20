83a7efd630087cbd33d699504a072fbccf3ca07c
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-20 17:20:43 -0500

    Adición de nuevos procedimientos
    Procedimiento para cambiar contraseñas

---
+++ UpdateProcedures.sql
@@ -0,0 +1,22 @@
+                          /* Procedimiento para cambiar contraseñas en la base de datos */
+DELIMITER $$
+CREATE PROCEDURE cambiar_contrasenia(
+    IN id INT(4),
+    IN pass VARCHAR(20))
+BEGIN    
+	UPDATE usuarios SET contrasenia = pass WHERE id_usuario = id;
+END $$
+
+                          /* Procedimiento para cambiar contraseñas en la base de datos */
+DELIMITER $$
+CREATE PROCEDURE update_user(
+	IN id INT(4),
+	IN name VARCHAR(25),
+    IN lname VARCHAR(25),
+    IN user_rol VARCHAR(15),
+    IN email VARCHAR(50),
+    IN pass VARCHAR(20))
+BEGIN
+	UPDATE usuarios set nombre = name, apellido = lname, rol = user_rol, 
+    correo = email, contrasenia = pass WHERE id_usuario = id;
+END $$

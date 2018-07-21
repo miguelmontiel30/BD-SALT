2b2c54f052bb52be766a2e9516f4bb507200b5c5
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-21 11:05:44 -0500

    Update UpdateProcedures.sql

--- UpdateProcedures.sql
+++ UpdateProcedures.sql
@@ -12,11 +12,10 @@ DELIMITER $$
 CREATE PROCEDURE update_user(
 	IN id INT(4),
 	IN name VARCHAR(25),
-    IN lname VARCHAR(25),
-    IN user_rol VARCHAR(15),
-    IN email VARCHAR(50),
-    IN pass VARCHAR(20))
+    	IN lname VARCHAR(25),
+    	IN user_rol VARCHAR(15),
+    	IN email VARCHAR(50))
 BEGIN
 	UPDATE usuarios set nombre = name, apellido = lname, rol = user_rol, 
-    correo = email, contrasenia = pass WHERE id_usuario = id;
+    correo = email WHERE id_usuario = id;
 END $$

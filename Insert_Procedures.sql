7e1f5cbbcd6f88924a7e034756ab3621bdf4e018
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-23 17:47:40 -0500

    Adición de nuevo Procedimiento
    Procedimiento para insertar viajes.

--- Insert_Procedures.sql
+++ Insert_Procedures.sql
@@ -11,3 +11,19 @@ BEGIN
     INSERT INTO usuarios(nombre,apellido,rol,correo,contrasenia)
     VALUES(name,last_name,rol,email,pass);
 END$$
+		     
+		     
+					/* Procedimiento para insertar viajes */
+DELIMITER $$
+CREATE PROCEDURE insert_travel(
+    IN f_salida date, 
+       dest varchar(20),
+       mot VARCHAR(35),       
+	obs VARCHAR(50),
+	id_ve int(8),
+	id_us int(4))
+BEGIN
+    INSERT INTO viajes(fecha_salida,destino,motivo,estado_viaje,
+		       observaciones,id_vehiculo,id_usuario)
+    VALUES(f_salida,dest,mot,'Iniciado',obs,id_ve,id_us);
+END$$

305a043fc8943f6e0c0ed5fa2a16f1f557eef475
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-31 23:07:49 -0500

    Adición de una nueva vista.
    Creacion de la vista *detalle viaje*

--- views.sql
+++ views.sql
@@ -1,7 +1,22 @@
+					/* Vista para la tabla viajes */
+					
 CREATE OR REPLACE VIEW vista_viajes
 AS SELECT viajes.id_viaje AS 'ID', viajes.fecha_salida AS 'FECHA_SALIDA', viajes.destino AS 'DESTINO',
 	viajes.estado_viaje AS 'ESTADO_VIAJE', viajes.motivo AS 'MOTIVO', viajes.observaciones AS 'OBSERVACIONES',
 	vehiculos.id_vehiculo AS 'ID_VEHICULO', vehiculos.modelo AS 'MODELO_VEHICULO', usuarios.id_usuario AS 'ID_USUARIO', usuarios.nombre AS 'CONDUCTOR'
+	FROM viajes
+	INNER JOIN vehiculos
+  ON vehiculos.id_vehiculo = viajes.id_vehiculo
+  INNER JOIN usuarios
+  ON usuarios.id_usuario = viajes.id_usuario;
+
+					/* Vista para los detalles del viaje */
+					
+CREATE OR REPLACE VIEW vista_detalle_viaje
+AS SELECT viajes.id_viaje, viajes.fecha_salida, viajes.destino, viajes.motivo, 
+	viajes.observaciones, vehiculos.marca, vehiculos.modelo, vehiculos.placa,
+	vehiculos.capacidad_tanque, vehiculos.rendimiento, vehiculos.vigencia_tarjeta,
+	vehiculos.kilometraje, usuarios.nombre
 	FROM viajes
 	INNER JOIN vehiculos
   ON vehiculos.id_vehiculo = viajes.id_vehiculo
